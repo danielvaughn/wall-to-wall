3874b23a38d19a9c23e07060690dd57501515345
Daniel Vaughn <daniel.vaughn@whiteops.com>
2017-10-15 12:03:54 -0400

    removes express file

--- app/router/index.js
+++ app/router/index.js
@@ -3,6 +3,10 @@ const express = require('express'),
     router = express.Router(),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    compress = require('compression'),
+    methodOverride = require('method-override'),
+    logger = require('morgan'),
     Promise = require('bluebird');
     Promise.promisifyAll(mongoose);
 
@@ -11,6 +15,14 @@ const controller = {
 };
 
 module.exports = function (app, config) {
+  var env = process.env.NODE_ENV || 'development';
+  app.locals.ENV = env;
+  app.locals.ENV_DEVELOPMENT = env == 'development';
+
+  app.use(logger('dev'));
+  app.use(cookieParser());
+  app.use(compress());
+  app.use(methodOverride());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
     extended: true
